e80aec9befeda4dc7bfb69187da3413c56c67c03
CompulsiveCoder <compulsivecoder@gmail.com>
2017-12-08 08:00:28 +0000

    Fixing testing

--- tests/nunit/init.sh
+++ tests/nunit/init.sh
@@ -1,2 +1,11 @@
 echo "Initializing nunit tests for SoilMoistureSensorCalibratedPump project"
 
+DIR=$PWD
+
+cd lib && \
+sh get-libs.sh && \
+cd $DIR && \
+
+cd lib/ArduinoSerialControllerClient && \
+sh init.sh
+cd $DIR
