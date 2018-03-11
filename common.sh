8a1dccc3b8e479e263f163a7c275fd8807bd85b9
CompulsiveCoder <compulsivecoder@gmail.com>
2018-03-11 08:15:20 +0000

    Updated common.sh

--- common.sh
+++ common.sh
@@ -9,3 +9,10 @@ if pio device list | grep -q 'ttyUSB2'; then
   IRRIGATOR_PORT="/dev/ttyUSB2"
   SIMULATOR_PORT="/dev/ttyUSB3"
 fi
+
+# If multiple devices are detected then this becomes the second device pair
+if pio device list | grep -q 'ttyUSB1'; then
+  IRRIGATOR_PORT="/dev/ttyUSB1"
+fi
+
+
