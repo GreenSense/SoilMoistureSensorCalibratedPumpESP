e89983a2daf679077815b3abd9f20224859be596
CompulsiveCoder <compulsivecoder@gmail.com>
2018-06-11 19:07:42 +1000

    Merged test upgrades into project

--- tests/nunit/lib/get-libs.sh
+++ tests/nunit/lib/get-libs.sh
@@ -5,15 +5,14 @@ LIB_DIR=$PWD
 
 NUGET_FILE="nuget.exe"
 
-cert-sync --quiet /etc/ssl/certs/ca-certificates.crt
-
 if [ ! -f "$NUGET_FILE" ];
 then
     echo "Getting nuget.exe..."
     wget http://nuget.org/nuget.exe -q
-    echo "Updating nuget.exe..."
-    mono nuget.exe update -self
 fi
+
+echo "Updating nuget.exe..."
+mono nuget.exe update -self
 
 if [ ! -d "NUnit.2.6.4" ]; then
     mono nuget.exe install nunit -version 2.6.4
@@ -23,6 +22,6 @@ if [ ! -d "NUnit.Runners.2.6.4" ]; then
     mono nuget.exe install nunit.runners -version 2.6.4
 fi
 
-if [ ! -d "ArduinoSerialControllerClient.1.0.7" ]; then
-    mono nuget.exe install ArduinoSerialControllerClient -version 1.0.7
+if [ ! -d "ArduinoSerialControllerClient.1.0.9" ]; then
+    mono nuget.exe install ArduinoSerialControllerClient -version 1.0.9
 fi
