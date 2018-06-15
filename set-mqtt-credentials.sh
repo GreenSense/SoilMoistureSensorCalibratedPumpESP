fd86e64cdecbe5d18e5922a5a540f969ef1f0f78
John <compulsivecoder@gmail.com>
2018-06-15 02:47:23 +0000

    Updated jenkinsfile and fixed script

--- set-mqtt-credentials.sh
+++ set-mqtt-credentials.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo ""
-echo "Setting mosquitto MQTT credentials..."
+echo "Setting MQTT credentials..."
 echo ""
 
 HOST=$1
@@ -9,14 +9,13 @@ USERNAME=$2
 PASSWORD=$3
 PORT=$4
 
-
 if [ ! "$PORT" ]; then
   PORT="1883"
 fi
 
 if [ "$PASSWORD" ]; then
 
-  echo "Host: $USERNAME"
+  echo "Host: $HOST"
   echo "Username: $USERNAME"
   echo "Port: $PORT"
 
@@ -25,24 +24,8 @@ if [ "$PASSWORD" ]; then
   echo $PASSWORD > "mqtt-password.security"
   echo $PORT > "mqtt-port.security"
 
-  CREDENTIALS_FILE="scripts/docker/mosquitto/data/mosquitto.userfile"
-
-  echo "$USERNAME:$PASSWORD" > $CREDENTIALS_FILE
-
-  echo "Setting credentials file:"
-  echo "  $CREDENTIALS_FILE"
-
-
-  echo ""
-  echo "Setting mqtt bridge config file:"
-  BRIDGE_SERVICE_CONFIG_FILE="scripts/apps/BridgeArduinoSerialToMqttSplitCsv/BridgeArduinoSerialToMqttSplitCsv/lib/net40/BridgeArduinoSerialToMqttSplitCsv.exe.config"
-  echo "  $BRIDGE_SERVICE_CONFIG_FILE"
-  sed -i "s/localhost/$HOST/g" $BRIDGE_SERVICE_CONFIG_FILE && \
-  sed -i "s/user/$USERNAME/g" $BRIDGE_SERVICE_CONFIG_FILE && \
-  sed -i "s/123456/$PASSWORD/g" $BRIDGE_SERVICE_CONFIG_FILE
-
   echo ""
   echo "Finished setting MQTT credentials"
 else
-  echo "Please provide username and password as arguments"
+  echo "Please provide host, username and password as arguments"
 fi
