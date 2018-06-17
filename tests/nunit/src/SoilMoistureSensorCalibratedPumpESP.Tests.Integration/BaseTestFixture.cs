fcfff348de2c3da23db772521724c747dc625682
CompulsiveCoder <compulsivecoder@gmail.com>
2018-06-17 12:52:04 +1000

    Fixed jenkinsfile

--- tests/nunit/src/SoilMoistureSensorCalibratedPumpESP.Tests.Integration/BaseTestFixture.cs
+++ tests/nunit/src/SoilMoistureSensorCalibratedPumpESP.Tests.Integration/BaseTestFixture.cs
@@ -7,7 +7,7 @@ namespace SoilMoistureSensorCalibratedPumpESP.Tests.Integration
 {
 	public class BaseTestFixture
 	{
-		public BaseTestFixture ()
+		public BaseTestFixture()
 		{
 		}
 
@@ -21,59 +21,59 @@ namespace SoilMoistureSensorCalibratedPumpESP.Tests.Integration
 		{
 		}
 
-        public string GetDevicePort()
+		public string GetDevicePort()
 		{
-			var devicePort = Environment.GetEnvironmentVariable ("IRRIGATOR_ESP_PORT");
-			
+			var devicePort = Environment.GetEnvironmentVariable("IRRIGATOR_ESP_PORT");
+
 			if (String.IsNullOrEmpty(devicePort))
 				devicePort = "/dev/ttyUSB2";
-			
-			Console.WriteLine ("Device port: " + devicePort);
-			
+
+			Console.WriteLine("Device port: " + devicePort);
+
 			return devicePort;
 		}
 
 		public string GetSimulatorPort()
 		{
-			var simulatorPort = Environment.GetEnvironmentVariable ("IRRIGATOR_ESP_SIMULATOR_PORT");
-			
+			var simulatorPort = Environment.GetEnvironmentVariable("IRRIGATOR_ESP_SIMULATOR_PORT");
+
 			if (String.IsNullOrEmpty(simulatorPort))
 				simulatorPort = "/dev/ttyUSB3";
-			
-			Console.WriteLine ("Simulator port: " + simulatorPort);
-			
+
+			Console.WriteLine("Simulator port: " + simulatorPort);
+
 			return simulatorPort;
 		}
 
 		public int GetDeviceSerialBaudRate()
 		{
-			var baudRateString = Environment.GetEnvironmentVariable ("IRRIGATOR_ESP_BAUD_RATE");
-			
+			var baudRateString = Environment.GetEnvironmentVariable("IRRIGATOR_ESP_BAUD_RATE");
+
 			var baudRate = 0;
-			
+
 			if (String.IsNullOrEmpty(baudRateString))
 				baudRate = 115200;
 			else
 				baudRate = Convert.ToInt32(baudRateString);
-			
-			Console.WriteLine ("Device baud rate: " + baudRate);
-			
+
+			Console.WriteLine("Device baud rate: " + baudRate);
+
 			return baudRate;
 		}
 
 		public int GetSimulatorSerialBaudRate()
 		{
-			var baudRateString = Environment.GetEnvironmentVariable ("IRRIGATOR_ESP_SIMULATOR_BAUD_RATE");
-			
+			var baudRateString = Environment.GetEnvironmentVariable("IRRIGATOR_ESP_SIMULATOR_BAUD_RATE");
+
 			var baudRate = 0;
-			
+
 			if (String.IsNullOrEmpty(baudRateString))
 				baudRate = 9600;
 			else
 				baudRate = Convert.ToInt32(baudRateString);
-			
-			Console.WriteLine ("Simulator baud rate: " + baudRate);
-			
+
+			Console.WriteLine("Simulator baud rate: " + baudRate);
+
 			return baudRate;
 		}
 	}
