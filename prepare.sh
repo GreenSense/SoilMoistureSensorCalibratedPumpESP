9bb64d97b894dad686a803a546029d25ec7f231a
CompulsiveCoder <compulsivecoder@gmail.com>
2018-07-08 11:08:29 +1000

    Fixed prepare script

--- prepare.sh
+++ prepare.sh
@@ -26,5 +26,5 @@ if ! type "msbuild" > /dev/null; then
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
   sudo echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 
-  sudo apt-get update && apt-get install -y mono-devel mono-complete ca-certificates-mono msbuild
+  sudo apt-get update && apt-get install -y --allow-unauthenticated mono-devel mono-complete ca-certificates-mono msbuild
 fi
