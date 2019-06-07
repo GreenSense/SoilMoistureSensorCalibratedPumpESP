30b3fcbedab5c6921b20bbb0da13ae170d10f79f
CompulsiveCoder <compulsivecoder@gmail.com>
2019-06-08 07:32:37 +1000

    Changed rollback script to only run in lts branch test failure

--- rollback.sh
+++ rollback.sh
@@ -2,7 +2,7 @@
 
 BRANCH=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
-if [ "$BRANCH" = "master" ]
+if [ "$BRANCH" = "lts" ]
 then
   echo "Rolling back the master branch to previous revision"
 
@@ -12,10 +12,8 @@ then
   echo "Pushing back to origin/master" && \
   git push --force origin master && \
 
-  echo "The 'master' branch has been rolled back"  || \
-
-  (echo "Error" && exit 1)
+  echo "The 'master' branch has been rolled back"  || exit 1
 else
-  echo "You must be in the 'master' branch to perform a rollback. The 'dev' branch doesn't get rolled back automatically."
+  echo "You must be in the 'lts' branch to perform a rollback. Rollback skipped."
 fi
 
